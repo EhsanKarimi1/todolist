d03f1621f743c890fdbc5188e13f4210bf43cdb8
Ehsan Karimi <karimib.ehsan@gmail.com>
2023-07-14 15:04:53 +0330

    add manifest and sw.js for pwa support

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = `ToDo-list`;
+
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll([
+      '/',
+      '/script.js',
+      '/style.css',
+      '/asset/fonts/Peyda-Medium.eot',
+      '/asset/fonts/Peyda-Medium.woff',
+      '/asset/fonts/Peyda-Medium.woff2',
+      '/asset/bars-icon.svg',
+      '/asset/icon.png',
+    ]);
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    // Get the resource from the cache.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          // If the resource was not in the cache, try the network.
+          const fetchResponse = await fetch(event.request);
+
+          // Save the resource in the cache and return it.
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // The network failed.
+        }
+    }
+  })());
+});
